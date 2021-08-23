866efedee556b6fc106594d8b0a37812764038df
kukuh-imanura <88927405+kukuh-imanura@users.noreply.github.com>
2021-08-23 17:02:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,165 +1,29 @@
 
 
+// TOGGLE
 
+const toggle = () => {
 
-// CALCULATION
+	let toggle = document.querySelector(".toggle");
+	toggle.classList.toggle("fa-toggle-on");
 
-let prevNumber = '';
-let calculationOperator = '';
-let currentNumber = '0';
+	let darkMode = document.querySelector(".darkMode");
+	darkMode.classList.toggle("fa-toggle-on");
 
+	let element = document.body;
+	element.classList.toggle("dark-mode");
 
-
-const inputNumbers = (number) => {
-
-	// currentNumber = number;
-	// currentNumber += number;
-
-	if (currentNumber === '0') {
-		currentNumber = number;
-	} else {
-		currentNumber += number;
-	}
 }
 
+// const darkMode = () => {
 
+// 	let darkMode = document.querySelector(".darkMode");
+// 	darkMode.classList.darkMode("fa-toggle-on");
 
-const operators = document.querySelectorAll(".operator");
+// 	let element = document.body;
+// 	element.classList.toggle("dark-mode");
 
-operators.forEach(
-	(operator) => {
-		operator.addEventListener("click", 
-			(event) => {
-				inputOperator(event.target.value);
-			}
-		)
-	}
-)
-
-
-
-const inputOperator = (operator) => {
-	if (calculationOperator === '') {
-		prevNumber = currentNumber;
-	}
-	calculationOperator = operator;
-	currentNumber = '';
-}
-
-
-
-const equalSign = document.querySelector(".equal-sign");
-
-equalSign.addEventListener("click", 
-	() => {
-		calculate();
-		updateScreen(currentNumber);
-	}
-)
-
-
-
-const calculate = () => {
-	let result = '';
-	switch(calculationOperator) {
-		case "+":
-			result = parseFloat(prevNumber) + parseFloat(currentNumber);
-			break;
-		case "-":
-			result = parseFloat(prevNumber) - parseFloat(currentNumber);
-			break;
-		case "*":
-			result = parseFloat(prevNumber) * parseFloat(currentNumber);
-			break;
-		case "/":
-			result = parseFloat(prevNumber) / parseFloat(currentNumber);
-			break;
-		default:
-			break;
-	}
-	currentNumber = result;
-	calculationOperator = '';
-}
-
-
-
-// DECIMAL CALCULATION
-
-const decimal = document.querySelector(".decimal");
-
-decimal.addEventListener("click", 
-	(event) => {
-		inputDecimal(event.target.value);
-		updateScreen(currentNumber);
-	}
-)
-
-
-
-inputDecimal = (dot) => {
-	if (currentNumber.includes('.')) {
-		return;
-	}
-	currentNumber += dot;
-}
-
-
-
-// CLEAR
-
-const clearBtn = document.querySelector(".all-clear");
-
-clearBtn.addEventListener("click", 
-	() => {
-		clearAll();
-		updateScreen(currentNumber);
-	}
-)
-
-
-
-const clearAll = () => {
-	prevNumber = '';
-	calculationOperator = '';
-	currentNumber = '0';
-}
-
-
-
-// UPDATE SCREEN
-
-const calculatorScreen = document.querySelector(".calculator-screen");
-
-const updateScreen = (number) => {
-	calculatorScreen.value = number;
-}
-
-
-
-const numbers = document.querySelectorAll(".number");
-
-// console.log(numbers);
- 
-// numbers.forEach(
-// 	(number) => {
-// 		console.log(number);
-// 	}
-// )
-
-numbers.forEach(
-	(number) => {
-		number.addEventListener("click",
-			(event) => {
-				inputNumbers(event.target.value);
-				updateScreen(currentNumber);
-			}
-		)
-		const calculatorScreen = document.querySelector(".calculator-screen");
-		// const updateScreen = (number) => {
-		// 	calculatorScreen.value = number;
-		// }
-	}
-)
+// }
 
 
 
